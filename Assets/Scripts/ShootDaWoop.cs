1666bd5787055353c012fc0afa8b1a697efa2c58
Vincent Van De Velde <fbampli@gmail.com>
2020-05-04 17:50:21 +0200

    Initial commit

---
+++ Assets/Scripts/ShootDaWoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ShootDaWoop : MonoBehaviour
+{
+
+    public GameObject bouboule;
+    public float speed = 100f;
+    public float dps = 3;
+
+    float timeLeft = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft <= 0f || Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject instBouboule = Instantiate(bouboule, transform.position, Quaternion.identity);
+            Rigidbody instBoubouleRigidBody = instBouboule.GetComponent<Rigidbody>();
+            instBoubouleRigidBody.AddForce(Vector3.forward * speed);
+        }
+    }
+}
